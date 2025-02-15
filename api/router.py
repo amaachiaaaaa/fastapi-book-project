a809787a31794925f90fa79fe0918482527237c9
Melisa <melamaachia@gmail.com>
2025-02-15 19:56:03 +0000

    chore setup github action

--- api/router.py
+++ api/router.py
@@ -1,13 +1,6 @@
-from fastapi import APIRouter,status, HTTPException
+from fastapi import APIRouter
 
 from api.routes import books
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
-
-@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-async def get_book(book_id: int):
-    book = db.get_book(book_id)
-    if not book:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return book
